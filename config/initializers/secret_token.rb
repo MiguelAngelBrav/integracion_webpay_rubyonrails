fe68319b389266c9bd01a066fbc8b5059040b177
Miguel Angel Bravo <miguel.bravo.r@gmail.com>
2013-05-27 17:05:52 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntegracionWebpayRubyonrails::Application.config.secret_token = '18245729295500e58975d5f20cd467155746bd2d7f996e4430e8fd99093917ddba2e11bb1849883d066a8bb05a02f264ccc20223dd989c4c1bd8bcbaf059183f'
