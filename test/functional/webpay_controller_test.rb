06b61aa6ac0ac95638da522c327903fba3e0f14a
Miguel Angel Bravo <miguel.bravo.r@gmail.com>
2013-07-09 18:30:09 -0400

    Se agrega test para probar check

--- test/functional/webpay_controller_test.rb
+++ test/functional/webpay_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 describe WebpayControllerTest do
-  it "check_mac" do
-    post :check
+	describe 'WebpayControllerTest' do
+  		it "check_mac" do
+    	post :check
 
-    response.status.must_equal 200
+    	response.status.must_equal 200
+	end
   end
 end
