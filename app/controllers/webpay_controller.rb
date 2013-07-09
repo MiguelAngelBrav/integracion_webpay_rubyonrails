703ed9e988ce175c303a314fc0b39f037ad442ef
Miguel Angel Bravo <miguel.bravo.r@gmail.com>
2013-07-09 15:50:39 -0400

    Se elimina ejecucion manual del cgi checkmac

--- app/controllers/webpay_controller.rb
+++ app/controllers/webpay_controller.rb
@@ -17,7 +17,6 @@ class WebpayController < ApplicationController
 	end
 
 	def check
-		run(env, payment_cgi_path)
 		Rails.logger.debug "\n***** check-request: #{request.inspect} *****\n"	
 		render :text => 'ACEPTADO', :layout => false
 	end
@@ -31,52 +30,6 @@ class WebpayController < ApplicationController
 	end
 
   private
-
-  # Extracted from rack-legacy
-  def run(env, *path)
-    status = 200
-    headers = {}
-    body = ''
-
-    Rails.logger.debug "\n***** Rails.root: #{Rails.root} *****\n"  
-
-    stderr = Tempfile.new 'legacy-rack-stderr'
-    IO.popen('-', 'r+') do |io|
-      if io.nil?  # Child
-        $stderr.reopen stderr.path
-        ENV['DOCUMENT_ROOT'] = Rails.root
-        ENV['SERVER_SOFTWARE'] = 'Rack Legacy'
-        env.each {|k, v| ENV[k] = v if v.respond_to? :to_str}
-        exec *path
-      else        # Parent
-        io.write(env['rack.input'].read) if env['rack.input']
-        io.close_write
-        until io.eof? || (line = io.readline.chomp) == ''
-          if line =~ /\s*\:\s*/
-            key, value = line.split(/\s*\:\s*/, 2)
-            if headers.has_key? key
-              headers[key] += "\n" + value
-            else
-              headers[key] = value
-            end
-          end
-        end
-        body = io.read
-        stderr.rewind
-        stderr = stderr.read
-        Process.wait
-        unless $?.exitstatus == 0
-          status = 500
-          body = ErrorPage.new(env, headers, body, stderr).to_s
-          headers = {'Content-Type' => 'text/html'}
-        end
-      end
-    end
-
-    status = headers.delete('Status').to_i if headers.has_key? 'Status'
-    [status, headers, [body]]
-  end
-
 
 
 end
