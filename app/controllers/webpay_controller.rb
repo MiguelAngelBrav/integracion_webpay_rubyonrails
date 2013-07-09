a9c3854898c7a80e65875a9f15a9c5dc83b6fc94
Miguel Angel Bravo <miguel.bravo.r@gmail.com>
2013-07-09 18:11:31 -0400

    Se agrega ejecucion cgi checkmac

--- app/controllers/webpay_controller.rb
+++ app/controllers/webpay_controller.rb
@@ -20,6 +20,9 @@ class WebpayController < ApplicationController
 	end
 
 	def check
+
+    user_agent     = "Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0a2"
+
     # parametros de la peticion
     host =  Rails.application.config.host_webserver_bridge
     request_params = "/wpi/cgi-bin/tbk_check_mac.cgi"
@@ -28,9 +31,9 @@ class WebpayController < ApplicationController
 
     # se arma la peticion al server remoto (que es asi mismo)
     response = Net::HTTP.start(host) do |http|
-      request            = Net::HTTP::Get.new("#{request_params}")
-      request["User-Agent"]      = URI.escape(request['HTTP_USER_AGENT'])
-      request["Accept"]        = "*/*"
+      request               = Net::HTTP::Get.new("#{request_params}")
+      request["User-Agent"] = URI.escape(user_agent)
+      request["Accept"]     = "*/*"
       request["Accept-Encoding"] = "gzip, deflate"
 
       http.request(request)
