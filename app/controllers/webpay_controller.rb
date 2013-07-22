87635965e4034d463045dc7b1235d4d578693e3a
Miguel Angel Bravo <miguel.bravo.r@gmail.com>
2013-07-22 18:37:11 -0400

    Se modifica ejecucion cgi checkmac

--- app/controllers/webpay_controller.rb
+++ app/controllers/webpay_controller.rb
@@ -19,37 +19,68 @@ class WebpayController < ApplicationController
 		Rails.logger.debug "\n<<<<< failure-request \n" 
 	end
 
-	def check
+	def check(env)
+  
+    result = process(env, check_cgi_path)
 
-    user_agent     = "Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20120716 Firefox/15.0a2"
-
-    # parametros de la peticion
-    host =  Rails.application.config.host_webserver_bridge
-    request_params = "/wpi/cgi-bin/tbk_check_mac.cgi"
-
-    Rails.logger.debug "<<<<< request: #{host}#{request_params}"
-
-    # se arma la peticion al server remoto (que es asi mismo)
-    response = Net::HTTP.start(host) do |http|
-      request               = Net::HTTP::Get.new("#{request_params}")
-      request["User-Agent"] = URI.escape(user_agent)
-      request["Accept"]     = "*/*"
-      request["Accept-Encoding"] = "gzip, deflate"
-
-      http.request(request)
-    end
-
-    # se carga la respueta en body
-    body = response.body
-
-    Rails.logger.debug "<<<<< body: #{body}"
+    Rails.logger.debug "<<<<< result: #{result}"
 
     Rails.logger.debug "\n<<<<< check-request \n"	
     render :text => 'ACEPTADO', :layout => false
   end
 
+  protected
 
-  private
+  # Extracted from rack-legacy
+  def process(env,cgi_path)
+    # raise 'Method not allowed' unless env['REQUEST_METHOD'] == 'POST'
+    
+    status = 200
+    headers = {}
+    body = ''
+
+    stderr = Tempfile.new 'webpay-cgi-stderr'
+    IO.popen('-', 'r+') do |io|
+      if io.nil?  # Child
+        $stderr.reopen stderr.path
+        ENV['DOCUMENT_ROOT'] = root_path
+        env.each {|k, v| ENV[k] = v if v.respond_to? :to_str}
+
+        exec ENV, cgi_path
+      else        # Parent
+        io.write(env['rack.input'].read) if env['rack.input']
+        io.close_write
+        until io.eof? || (line = io.readline.chomp) == ''
+          if line =~ /\s*\:\s*/
+            key, value = line.split(/\s*\:\s*/, 2)
+            if headers.has_key? key
+              headers[key] += "\n" + value
+            else
+              headers[key] = value
+            end
+          end
+        end
+        body = io.read
+        stderr.rewind
+        stderr = stderr.read
+        Process.wait
+        unless $?.exitstatus == 0
+          raise CGIExecutionError
+        end
+      end
+    end
+
+    status = headers.delete('Status').to_i if headers.has_key? 'Status'
+    [status, headers, [body]]
+  end
+
+  def root_path
+    Rails.root.join("vendor", "webpay").to_s
+  end
+  
+  def check_cgi_path
+    root_path + '/tbk_check_mac.cgi'
+  end
 
 
 end
