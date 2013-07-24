e2a24ec12241d068c561d06378d9fe0f7512f517
Miguel Angel Bravo <miguel.bravo.r@gmail.com>
2013-07-24 13:05:47 -0400

    Nuevo metodo para check-mac

--- app/controllers/webpay_controller.rb
+++ app/controllers/webpay_controller.rb
@@ -27,9 +27,9 @@ class WebpayController < ApplicationController
 
     Rails.logger.debug "<<<<< comienza parseo"
     
-    parse(request.raw_post)
+    valid_mac(request.raw_post)
     
-    Rails.logger.debug "<<<<< parametros parseados: #{@raw}"
+    Rails.logger.debug "<<<<< @valid_mac: #{@valid_mac}"
 
     Rails.logger.debug "<<<<< check-request"	
 
@@ -38,30 +38,21 @@ class WebpayController < ApplicationController
   end
 
   private
-
-  # Take the posted data and move the relevant data into a hash
-  def parse(post)
-    @raw = post
-    for line in post.split('&')
-      key, value = *line.scan( %r{^(\w+)\=(.*)$} ).flatten
-      params[key] = value
-    end
-  end
   
-  # def valid_mac
-  #   if @valid_mac.nil?
-  #     file = Tempfile.new 'webpay-mac-check'
-  #     file.write raw
-  #     file.close
-  #     executable = Webpay.cgis_root + '/tbk_check_mac.cgi'
-  #     @valid_mac = ( `#{executable} #{file.path}`.strip == VALID_MAC_RESPONSE )
-  #     file.unlink
+  def valid_mac(raw)
+    if @valid_mac.nil?
+      file = Tempfile.new 'webpay-mac-check'
+      file.write raw
+      file.close
+      executable = check_cgi_path
+      @valid_mac = ('#{executable} #{file.path}'.strip == VALID_MAC_RESPONSE)
+      file.unlink
       
-  #     fail! 'Mac Invalido' unless @valid_mac
-  #   end
+      fail! 'Mac Invalido' unless @valid_mac
+    end
     
-  #   @valid_mac
-  # end
+    @valid_mac
+  end
 
   def root_path
     Rails.root.join("vendor", "webpay").to_s
