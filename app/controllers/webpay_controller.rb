46e8e31bb98cde8aa31e7253b0a4d0ecd1b7f5e9
Miguel Angel Bravo <miguel.bravo.r@gmail.com>
2013-07-24 17:03:23 -0400

    Nuevo metodo para check-mac

--- app/controllers/webpay_controller.rb
+++ app/controllers/webpay_controller.rb
@@ -25,32 +25,71 @@ class WebpayController < ApplicationController
 		Rails.logger.debug "\n<<<<< failure-request \n" 
 	end
 
+
 	def check
 
     Rails.logger.debug "<<<<< comienza parseo"
     
-    valid_mac(request.raw_post)
+    result = valida_mac(ENV, check_cgi_path, temp_file_param)
     
-    Rails.logger.debug "<<<<< @valid_mac: #{@valid_mac}"
-
-    Rails.logger.debug "<<<<< check-request"	
-
+    Rails.logger.debug "<<<<< result: #{result}"
 
     render :text => 'ACEPTADO', :layout => false
   end
 
   private
   
-  def valid_mac(raw)
-  
+  # crea archivo temporal y guarda path
+  def temp_file_param(raw)  
     file = Tempfile.new 'webpay-mac-check'
     file.write raw
     file.close
-    executable = check_cgi_path
-    @valid_mac = '#{executable} #{file.path}'.strip
+    path_file = file.path
     file.unlink
-    
-    @valid_mac
+        
+    path_file
+  end
+
+  def valida_mac(env, *path, path_param)
+    status = 200
+    headers = {}
+    body = ''
+
+    stderr = Tempfile.new 'webpay-cgi-stderr'
+    IO.popen('-', 'r+') do |io|
+      if io.nil?  # Child
+        $stderr.reopen stderr.path
+        ENV['DOCUMENT_ROOT'] = root_path
+        ENV['SERVER_SOFTWARE'] = 'Rack Legacy'
+        env.each {|k, v| ENV[k] = v if v.respond_to? :to_str}
+        exec *path + " " + path_param
+      else        # Parent
+        io.write(env['rack.input'].read) if env['rack.input']
+        io.close_write
+        until io.eof? || (line = io.readline.chomp) == ''
+          if line =~ /\s*\:\s*/
+            key, value = line.split(/\s*\:\s*/, 2)
+            if headers.has_key? key
+              headers[key] += "\n" + value
+            else
+              headers[key] = value
+            end
+          end
+        end
+        body = io.read
+        stderr.rewind
+        stderr = stderr.read
+        Process.wait
+        unless $?.exitstatus == 0
+          status = 500
+          body = ErrorPage.new(env, headers, body, stderr).to_s
+          headers = {'Content-Type' => 'text/html'}
+        end
+      end
+    end
+
+    status = headers.delete('Status').to_i if headers.has_key? 'Status'
+    [status, headers, [body]]
   end
 
   def root_path
